f0950338db786d272e1730fe5733cfef4ec80c7d
Veer <vtube258@gmail.com>
2022-10-28 22:33:34 +0530

    Center Header w/ RGB

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Header.css"
+
+function Header(){
+    return (
+        <div className="header">
+            <h1>Hi, I'm Veer</h1>
+            <h3>I'm a Student, Digital Artist, Web Developer and Programmer</h3>
+        </div>
+    )
+}
+
+export default Header
