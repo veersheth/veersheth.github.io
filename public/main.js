c74fbc13dc41ac2219f23f74cc078cfc8825d288
Veer <vtube258@gmail.com>
2022-11-02 14:33:11 +0530

    Fix bounce animation function not running on website startup

--- public/main.js
+++ public/main.js
@@ -5,3 +5,38 @@ const cursor = new MouseFollower({
     className: 'mf-cursor',
 });
 
+function spanHeaders(){
+    const myHeaders = document.querySelectorAll("h1")
+  
+    myHeaders.forEach(header => {
+        const headerText = header.innerText
+        header.innerHTML = ''
+        headerText.split('').forEach(character => {
+            const characterSpan = document.createElement('span')
+            if (character === ' ') {
+              characterSpan.innerHTML = "&nbsp;"
+            }  
+            else { 
+              characterSpan.innerText = character 
+            }
+            header.appendChild(characterSpan) 
+        }) 
+    })
+  
+    document.querySelectorAll("span").forEach(span => {
+        span.addEventListener( "mouseover", () => {
+            console.log("hover");
+            span.classList.add("bounce-now")
+            setTimeout(function() {
+              span.classList.remove("bounce-now")
+              //your code to be executed after 1 second
+            }, 700);
+            // span.classList.toggle("bounce-now")
+        })
+    })
+  }
+  
+//   the function does not run by itself no matter which .js file I place it at, so this function was made to run it after a half second
+  setTimeout(function() {
+    spanHeaders()
+  }, 500);
--- src/App.js
+++ src/App.js
@@ -6,43 +6,13 @@ import Slide2 from "./components/Slide2"
 
 function App() {
 
-  function spanHeaders(){
-    const myHeaders = document.querySelectorAll("h1")
-  
-    myHeaders.forEach(header => {
-        const headerText = header.innerText
-        header.innerHTML = ''
-        headerText.split('').forEach(character => {
-            const characterSpan = document.createElement('span')
-            if (character === ' ') {
-              characterSpan.innerHTML = "&nbsp;"
-            }
-            else { 
-              characterSpan.innerText = character 
-            }
-            header.appendChild(characterSpan) 
-        }) 
-    })
-  
-    document.querySelectorAll("span").forEach(span => {
-        span.addEventListener( "mouseover", () => {
-            console.log("hover");
-            span.classList.add("bounce-now")
-            setTimeout(function() {
-              span.classList.remove("bounce-now")
-              //your code to be executed after 1 second
-            }, 700);
-            // span.classList.toggle("bounce-now")
-        })
-    })
-  }
+
   
 
   return (
     <div className="App">
       <Slide1/>
       <Slide2/>
-      {spanHeaders()}
     </div>
   );
 }
