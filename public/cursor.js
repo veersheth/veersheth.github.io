b4cbb584ad108f9c833fc2d292d085495b019589
Veer Sheth <veerksheth@gmail.com>
2023-12-03 23:16:13 +1100

    set theme based on user device theme

--- public/cursor.js
+++ public/cursor.js
@@ -1,48 +1,37 @@
-
 function cursorFunc() {
-    const trailer = document.querySelector(".trailer");
+    // Get the trailer element
+    let trailer = document.querySelector(".trailer");
 
+    // Function to animate the trailer
     const animateTrailer = (e, interacting, interactable) => {
-        const x = e.clientX - trailer.offsetWidth / 2,
+        let x = e.clientX - trailer.offsetWidth / 2,
             y = e.clientY - trailer.offsetHeight / 2;
 
-            const moveframes = {
-                transform: `translate(${x}px, ${y}px) scale(${interacting ? 6 : 1})`,
-                opacity:1,
-            }
-
-            if(interacting){
-                const hoverValue = interactable.dataset.hover;
-                trailer.innerHTML = hoverValue
-            }else{
-                trailer.innerHTML = ""
-            }
-
-            trailer.animate(moveframes, { 
-                duration: 300, 
-                fill: "forwards" 
-                });
+        let moveframes = {
+            transform: `translate(${x}px, ${y}px) scale(${interacting ? 6 : 1})`,
+            opacity:1,
         }
 
-    window.onmousemove = e => {
-        const interactable = e.target.closest(".interactable"),
-              interacting = interactable !== null;
+        if(interacting){
+            let hoverValue = interactable.dataset.hover;
+            trailer.innerHTML = hoverValue
+        }else{
+            trailer.innerHTML = ""
+        }
+
+        trailer.animate(moveframes, { 
+            duration: 300, 
+            fill: "forwards" 
+        });
+    }
+
+    // Add event listener for mouse movement
+    window.addEventListener('mousemove', e => {
+        let interactable = e.target.closest(".interactable"),
+            interacting = interactable !== null;
         
         animateTrailer(e, interacting, interactable);
-    }
+    });
 }
 
 setTimeout(cursorFunc, 100);
-
-
-
-
-
-
-
-
-
-
-
-
-
